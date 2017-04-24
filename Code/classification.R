3548c852b26610b1c6684f367b89c4a0bfa17a64
Karthikeya212 <saidulla@iu.edu>
2017-04-24 17:54:29 -0400

    Decision Tree Code

--- Code/classification.R
+++ Code/classification.R
@@ -1,3 +1,10 @@
-f <-file("page-blocks.data", open="r" ,encoding="UTF-16LE")
-data <- read.table(f, dec=",", header=F)
-data
+library('rpart')
+library('rpart.plot')
+data = read.table("../Data/page-blocks.data", sep = "")
+data = as.matrix(data)
+fit =  rpart(data[,11] ~ data[,1:10], data=as.data.frame(data))
+rpart.plot(fit)
+
+class.pred <- table(predict(fit, type="class"), data[,11]) #confusion matrix
+cerror1 = 1-sum(diag(class.pred))/sum(class.pred) #classification error
+cerror1
