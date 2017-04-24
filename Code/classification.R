8692ba18b602a7f9de2934839bf7b1fb083586d8
Harshit Shrivastava <harshit.shrivastava14@gmail.com>
2017-04-24 18:48:48 -0400

    Trying out separate test and train datasets on rpart

--- Code/classification.R
+++ Code/classification.R
@@ -1,7 +1,22 @@
 library('rpart')
 library('rpart.plot')
-data = read.table("../Data/page-blocks.data", sep = "")
-data = as.matrix(data)
+dataset = read.table("../Data/page-blocks.data", sep = "")
+dataset = as.matrix(dataset)
+
+##experiments start here
+
+bound <- floor((nrow(dataset)/4)*3)         #define % of training and test set
+
+dataset <- dataset[sample(nrow(dataset)), ]           #sample rows 
+trainset <- dataset[1:bound, ]              #get training set
+testset <- dataset[(bound+1):nrow(dataset), ]    #get test set
+
+model <- rpart(trainset[,11] ~ trainset[,1:10], data = as.data.frame(trainset), minbucket=5)
+pred <- predict(model, data = as.data.frame(testset))
+
+##experiments end here
+
+
 fit =  rpart(data[,11] ~ data[,1:10], data=as.data.frame(data))
 rpart.plot(fit)
 
