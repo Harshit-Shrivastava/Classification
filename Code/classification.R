d42b391116ce66a5215fcfa996feb318d69f3744
Harshit Shrivastava <harshit.shrivastava14@gmail.com>
2017-04-25 18:28:14 -0400

    Changing the plotting of the decision tree

--- Code/classification.R
+++ Code/classification.R
@@ -3,10 +3,8 @@ library('rpart.plot')
 dataset = read.table("../Data/page-blocks.data", sep = "")
 dataset = as.matrix(dataset)
 
-##experiments start here
-
-bound <- floor((nrow(dataset)/4)*3)         #define % of training and test set
-
+#dividing the testing and training dataset into half-and-half
+bound <- floor((nrow(dataset)/2))
 dataset <- dataset[sample(nrow(dataset)), ]           #sample rows 
 trainset <- dataset[1:bound, ]              #get training set
 testset <- dataset[(bound+1):nrow(dataset), ]    #get test set
@@ -15,8 +13,8 @@ testset <- dataset[(bound+1):nrow(dataset), ]    #get test set
 model <- rpart(trainset[,11] ~ trainset[,1:10], data = as.data.frame(trainset), minbucket=5)
 
 #plotting the tree, along with attributes and split factors
-plot(model)
-text(model)
+rpart.plot(model)
+datatext(model)
 
 #printing and plotting relative error vs cp and size of tree for pruning
 printcp(model)
@@ -27,13 +25,4 @@ pred <- predict(model, data = as.data.frame(testset))
 
 #calculating accuracy
 accuracy = 1 - mean(pred == testset[ , testset[,11]]) 
-
-##experiments end here
-
-
-#fit =  rpart(data[,11] ~ data[,1:10], data=as.data.frame(data))
-#rpart.plot(fit)
-
-#class.pred <- table(predict(fit, type="class"), data[,11]) #confusion matrix
-#cerror1 = 1-sum(diag(class.pred))/sum(class.pred) #classification error
-#cerror1
+accuracy
